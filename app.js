8c944b56c7453d7bf1b39ed897b7ef69515d8afc
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-06-27 18:22:03 -0700

    working on it, need the to clear the pictures out, now

--- app.js
+++ app.js
@@ -1,31 +1,66 @@
 'use strict';
 
 var imgArray = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
-var displayArray = [];
-var tallyArray = [];
+var previousDisplay = [];
+var currentDisplay = [];
+var tallyArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var turns = 0;
 var products = document.getElementById('products');
 
 function makingPicture() {
   var randomNum = Math.floor(Math.random() * 19);
   var i = 0;
   var match = false;
-  while(!match && i < displayArray.length) {
-    if(randomNum === displayArray[i]) {
+  while(!match && i < currentDisplay.length) {
+    if(imgArray[randomNum] === currentDisplay[i]) {
       match = true;
     }else {
       i++;
     }
   }
   if(!match) {
-    displayArray.push(randomNum);
     var productImg = document.createElement('img');
     productImg.src = imgArray[randomNum];
     products.appendChild(productImg);
+    currentDisplay.push(imgArray[randomNum]);
   }else {
     makingPicture();
   }
 };
 
-makingPicture();
+function checkDisplay() {
+  makingPicture();
+  var j = 0;
+  while(j < previousDisplay.length) {
+    for(var i = 0; i < currentDisplay.length; i++) {
+      if(currentDisplay[i] === previousDisplay[j]) {
+        currentDisplay = [];
+        console.log(i);
+      }
+      j++;
+      console.log(j);
+    }
+  }
+};
 
-products.addEventListener('click', makingPicture);
+function moveArrays() {
+  currentDisplay = previousDisplay;
+  previousDisplay = [];
+};
+
+function runEverything() {
+  if(turns < 25) {
+    checkDisplay();
+    checkDisplay();
+    checkDisplay();
+    turns++;
+  }else{
+    products.removeEventListener();
+  }
+  console.log(currentDisplay);
+  console.log(previousDisplay);
+};
+
+runEverything();
+
+products.addEventListener('click', runEverything);
